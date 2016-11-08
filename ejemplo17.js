f69a78d5286ce1fee60b0fcddf8e1497f6062fde
jhonnpc04 <jhony.jp2gmail.com>
2016-11-08 19:28:56 -0400

    start

---
+++ ejemplo17.js
@@ -0,0 +1,4 @@
+var pelicula={titulo:"xmen", genero:"accion",director:"marvel",durecion:150}
+console.log("pelicula:" + pelicula.titulo);
+console.log("genero:" + pelicula.genero);
+console.log("director:" + pelicula.director);
